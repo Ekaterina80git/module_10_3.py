c74738e4a274e08980079517ad05d06b46eab988
Ekaterina80git <timofeewa.yekat@yandex.ru>
2024-11-21 17:10:18 +0300

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -3,33 +3,41 @@ import random
 import time
 import threading
 
+
 class Bank:
     def __init__(self):
         self.balance = 0
         self.lock = threading.Lock()
+        self.count = 0
         self.transaction = 100
 
     def deposit(self):
         for i in range(self.transaction):
+            self.count += 1
             amount = random.randint(50,500)
-            self.balance += amount
-            print(f'Пополнение: {amount}. Баланс: {self.balance}.')
             if self.balance >= 500 and self.lock.locked():
                self.lock.release()
-               time.sleep(0.001)
+            elif self.balance < 500 and self.lock.locked():
+                self.lock.release()
+                self.balance += amount
+                self.lock.acquire()
+            else:
+                self.balance += amount
+            print(f'Пополнение: {amount}. Баланс: {self.balance}.')
+            time.sleep(0.001)
 
     def take(self):
         for i in range(self.transaction):
             amount = random.randint(50, 500)
             print(f'Запрос на {amount}')
-            #self.lock.acquire()
+            self.lock.acquire()
             if amount <= self.balance:
                 self.balance -= amount
                 print(f'Снятие: {amount}. Баланс: {self.balance}.')
-            else:
+            elif amount > self.balance:
                 print('Запрос отклонён, недостаточно средств')
                 self.lock.acquire()
-                time.sleep(0.001)
+            time.sleep(0.001)
 
 
 
